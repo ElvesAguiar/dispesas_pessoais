ef211b6de6789da8a53af0acba959ef5af7d7c48
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-18 17:57:32 -0300

    feat: seed for transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,55 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'models/transaction.dart';
 
-void main() => runApp(ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-
+  const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:MyHomePage()
-
-    );
-    
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      't1',
+      'Novo Tênis de Corrida',
+      310.76,
+      DateTime.now(),
+    ),
+    Transaction(
+      't2',
+      'Conta de Luz',
+      211.30,
+      DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas pessoais'),
-      ),body: Center(child: Text('versão Inicial'),),
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          SizedBox(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
     );
-    
   }
 }
