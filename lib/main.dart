f305d91b94436c18bc1fb39fd26e0c06cbbcef93
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-19 10:43:35 -0300

    feat: using text controller

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,11 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+
+
+ final titleController = TextEditingController();
+ final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       't1',
@@ -95,16 +99,21 @@ class MyHomePage extends StatelessWidget {
               padding: const EdgeInsets.all(10),
               child: Column(children: [
                 TextField(
+                  controller: titleController,
                   decoration: InputDecoration(labelText: 'Título'),
                 ),
                 TextField(
+                  controller: valueController,
                   decoration: InputDecoration(labelText: 'Valor (R\$)'),
                 ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  print(titleController.text);
+                  print(valueController.text);
+                },
                 child: Text('Nova Transação'),
                 textColor: Colors.purple,
               ),
